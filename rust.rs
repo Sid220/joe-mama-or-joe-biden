8aea91de262f67663be14f134904dee14fd22e47
Sid220 <74916637+Sid220@users.noreply.github.com>
2021-07-28 20:32:32 -0400

    Create rust.rs

---
+++ rust.rs
@@ -0,0 +1,13 @@
+use rand::distributions::{Distribution, Uniform}; // 0.6.5
+
+fn main() {
+    let step = Uniform::new(0, 2);
+    let mut rng = rand::thread_rng();
+    let choice = step.sample(&mut rng);
+    if choice == 0{
+        println!("Joe Mama")
+    }
+    else {
+        println!("Joe Biden")
+    }
+}
