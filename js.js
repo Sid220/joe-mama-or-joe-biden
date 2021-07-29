397fe0ebdef69daa1d1f2fe0a06254cb3987d8ae
Sid220 <74916637+Sid220@users.noreply.github.com>
2021-07-28 20:37:27 -0400

    Create js.js

---
+++ js.js
@@ -0,0 +1,14 @@
+// Author: Adam
+var joe = Math.random() < 0.5;
+
+if (joe <= 0.5){
+  joe = true;
+} else{
+  joe = false;
+}
+
+if (joe == true) {
+console.log('Joe Mama');
+} else {
+  console.log('Joe Biden');
+}
