89d6fd2ed132327824af78b24ced86940daeef10
Sid220 <74916637+Sid220@users.noreply.github.com>
2021-07-29 12:07:09 -0400

    Create java.java

---
+++ java.java
@@ -0,0 +1,10 @@
+// Author: Adam
+public class joemama {
+    public static void main(String[] args) { 
+        if (Math.random() < 0.5){
+        System.out.println("Joe mama");
+        }else{
+            System.out.println("Joe Biden");
+        }
+}
+}
